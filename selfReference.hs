3181280891dbff0c1fa694a7e5c17b781106bcdf
waternotvodka <113853122+waternotvodka@users.noreply.github.com>
2022-11-18 21:24:05 +0000

    Update selfReference.hs

--- selfReference.hs
+++ selfReference.hs
@@ -6,11 +6,12 @@ factorials = 1:zipWith (*) factorials [1..]
 fibs :: [Int]
 fibs = 0:1:zipWith (+) fibs (tail fibs)
 
-powersOfTwo :: [Int]
-powersOfTwo = 1:map (*2) powersOfTwo
-
 powersOfN :: (Integral a) => a -> [a]
 powersOfN n = 1: map (*n) (powersOfN n)
+
+powersOfTwo :: [Int]
+powersOfTwo = powersOfN 2
+
 
 naturalNumbers :: [Int]
 naturalNumbers = 1 : map succ naturalNumbers
